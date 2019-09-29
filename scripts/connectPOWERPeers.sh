5d28894d94ac7d0ded2c1249680b65c799078efc
Gokul Rangarajan <gokulraja2006@gmail.com>
2019-09-29 14:12:01 +0530

    Updated channel join for reliance power

--- scripts/connectPOWERPeers.sh
+++ scripts/connectPOWERPeers.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 export CHANNEL_NAME=reliancenetworkchannel
 
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/power.reliance-network.com/users/Admin@power.reliance-network.com/msp
-CORE_PEER_ADDRESS=peer0.power.reliance-network.com:7051
-CORE_PEER_LOCALMSPID="powerMSP"
-CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/power.reliance-network.com/peers/peer0.power.reliance-network.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/communication.reliance-network.com/users/Admin@communication.reliance-network.com/msp
+CORE_PEER_ADDRESS=peer0.communication.reliance-network.com:7051
+CORE_PEER_LOCALMSPID="communicationMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/communication.reliance-network.com/peers/peer0.communication.reliance-network.com/tls/ca.crt
 
 peer channel join -b $CHANNEL_NAME.block
 sleep 10
-# peer channel update -o orderer.power.reliance-network.com:7050 -c $CHANNEL_NAME -f ../channel-artifacts/${CORE_PEER_LOCALMSPID}
+# peer channel update -o orderer.communication.reliance-network.com:7050 -c $CHANNEL_NAME -f ../channel-artifacts/${CORE_PEER_LOCALMSPID}
 
-CORE_PEER_ADDRESS=peer1.power.reliance-network.com:7051
-CORE_PEER_LOCALMSPID="powerMSP"
-CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/power.reliance-network.com/peers/peer1.power.reliance-network.com/tls/ca.crt
+CORE_PEER_ADDRESS=peer1.communication.reliance-network.com:7051
+CORE_PEER_LOCALMSPID="communicationMSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/communication.reliance-network.com/peers/peer1.communication.reliance-network.com/tls/ca.crt
 
 peer channel join -b $CHANNEL_NAME.block
 sleep 10
-# peer channel update -o orderer.power.reliance-network.com:7050 -c $CHANNEL_NAME -f ../channel-artifacts/${CORE_PEER_LOCALMSPID}
+# peer channel update -o orderer.communication.reliance-network.com:7050 -c $CHANNEL_NAME -f ../channel-artifacts/${CORE_PEER_LOCALMSPID}
