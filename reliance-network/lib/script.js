324cbdfdfe382a33f0fe9d6504af1783a2039054
Gokul Rangarajan <gokulraja2006@gmail.com>
2019-10-02 22:27:27 +0530

    added threshold transaction

--- reliance-network/lib/script.js
+++ reliance-network/lib/script.js
@@ -24,7 +24,81 @@
  * @transaction
  */
 async function AccelerationReading(tx) {
-    const assetRegistry = await getAssetRegistry('com.reliance.network.SampleAsset');
+    const contractRegistry = await getContractRegistry();
+    const shipmentRegistry = await getShippmentRegistry();
+    let shippment = tx.shipment;
+    let contract = contractRegistry.get(shippment.contract.getIdentifier());
+    /**
+     * Acceleration Threshold
+     * emit if accelratex < 10.4 and accelratey < 10.2 and accelratez < 10
+     */
+    if (10.4 > tx.accelerationX && 10.2 > tx.accelerationY && 10 > tx.accelerationZ) {
+        let accelerationThresholdEvent = getAccelerationThreshold();
+        accelerationThresholdEvent.accelerationX = tx.accelerationX;
+        accelerationThresholdEvent.accelerationY = tx.accelerationY;
+        accelerationThresholdEvent.accelerationZ = tx.accelerationZ;
+        accelerationThresholdEvent.latitude = tx.latitude;
+        accelerationThresholdEvent.longitude = tx.longitude;
+        accelerationThresholdEvent.readingTime = tx.readingTime;
+        accelerationThresholdEvent.message = "Acceleration is out of range";
+        emit(accelerationThresholdEvent);
+    }
+    shippment.accelerationReading.push(tx);
+    await shipmentRegistry.update(shippment);
+}
+
+/**
+ * Get contract registry
+ * @returns {AssetRegistry} contractRegistry
+ */
+async function getContractRegistry() {
+    const contractRegistry = await getAssetRegistry('com.reliance.network.Contract');
+    return contractRegistry;
+}
+
+/**
+ * Get shipment registry
+ * @returns {AssetRegistry} shipmentRegistry
+ */
+async function getShippmentRegistry()  {
+    const shipmentRegistry = await getAssetRegistry('com.reliance.network.Shipment');
+    return shipmentRegistry;
+}
+
+/**
+ * Get shipper registry
+ * @returns {ParicipantRegistry} shipperRegistry
+ */
+async function getShipperRegistry() {
+    const shipperRegistry = await getAssetRegistry('com.reliance.network.Shipper');
+    return shipperRegistry;
+}
+
+/**
+ * Get exporter registry
+ * @returns {ParicipantRegistry} exporterRegistry
+ */
+async function getExporterRegistry() {
+    const paricipantRegistry = await getAssetRegistry('com.reliance.network.Exporter');
+    return paricipantRegistry;
+}
+
+/**
+ * Get importer registry
+ * @returns {ParicipantRegistry} importerRegistry
+ */
+async function getImporterRegistry() {
+    const importerRegistry = await getAssetRegistry('com.reliance.network.Importer');
+    return importerRegistry;
+}
+
+/**
+ * getAccelerationThreshold event
+ * @returns {Event} AccelerationThreshold
+ */
+async function getAccelerationThreshold() {
+    let event = getFactory().newEvent('com.reliance.network', 'AccelerationThreshold');
+    return event;
 }
 
 /**
